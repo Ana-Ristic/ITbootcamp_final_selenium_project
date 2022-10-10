2d2b4d736ad102ceba88bff3848e800bfae53068
Ana Ristic <anu.mymail@gmail.com>
2022-10-10 22:00:32 +0200

    final selenium project update

--- src/pages/CitiesPage.java
+++ src/pages/CitiesPage.java
@@ -1,6 +1,9 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CitiesPage {
@@ -11,5 +14,54 @@ public class CitiesPage {
 		this.driver = driver;
 		this.wait = wait;
 	}
+	public WebElement getNewItemInput() {
+		return driver.findElement(By.id("name"));
+	}
+	public WebElement getNewItemButton() {
+		return driver.findElement(By.className("btnNewItem"));
+	}
+	public WebElement getSearchInput() {
+		return driver.findElement(By.id("search"));
+	}
+	public WebElement getMessageDialogText() {
+		return driver.findElement(By.xpath("//*[contains(text(), 'Saved successfully')]"));
+	}
+	
+	public void waitForMessageDialogToBeVisible() {
+		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
+				By.xpath("//*[contains(text(), 'Saved successfully')]")));
+	}
+	
+	public void waitForEditDialogtoBeVisible() {
+		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
+				By.className("dlgNewEditItem")));
+	}
+	public void waitForDeletePopUp() {
+		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
+				By.className("body-1")));
+	}
+	public WebElement getSaveButtonFromDialog() {
+		return driver.findElement(By.className("btnSave"));
+	}
+	public WebElement getDeleteButtonFromDialog() {
+		return driver.findElement(By.xpath("//div[contains(@class, 'v-card__actions')/button[2]"));
+	}
+	public void waitForNumberOfRows(int rowNumber) {
+		wait.until(ExpectedConditions.numberOfElementsToBe(By.xpath("//tbody/tr"), rowNumber));
+	}
+	public WebElement getTableCell(int rowNumber, int columnNumber) {
+		return driver.findElement(By.xpath("//tbody/tr["+ rowNumber + "]/td[" + columnNumber + "]"));
+	}
+	public WebElement getEditButton(int rowNumber) {
+		return driver.findElement(By.xpath("//tbody/tr["+ rowNumber + "]//button[@id = 'edit']"));
+	}
+	
+	public WebElement getDeleteButtonFromRow(int rowNumber) {
+		return driver.findElement(By.xpath("//tbody/tr["+ rowNumber + "]//div/button[2]"));
+	}
+	
+	
+	
+	
 
 }
--- src/tests/AdminCitiesTests.java
+++ src/tests/AdminCitiesTests.java
@@ -1,5 +1,79 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class AdminCitiesTests extends BasicTest{
+		@Test
+		public void VisitsTheAdminCitiesPageAndListCities() {
+			navPage.getLoginButton().click();
+			loginPage.getEmailInput().sendKeys("admin@admin.com");
+			loginPage.getPasswordInput().sendKeys("12345");
+			loginPage.getLoginButton().click();
+			navPage.getAdminButton().click();
+			navPage.getCitiesLink().click();
+			Assert.assertEquals(driver.getCurrentUrl(), baseUrl + "/admin/cities", "ERROR: Incorrect URL.");
+		}
 		
+		@Test
+		public void ChecksInputTypesForCreateEditNewCity() {
+//			navPage.getLoginButton().click();
+			loginPage.getEmailInput().sendKeys("admin@admin.com");
+			loginPage.getPasswordInput().sendKeys("12345");
+			loginPage.getLoginButton().click();
+			navPage.getAdminButton().click();
+			navPage.getCitiesLink().click();
+			citiesPage.getNewItemButton().click();
+			citiesPage.waitForEditDialogtoBeVisible();
+			Assert.assertEquals(citiesPage.getNewItemInput().getAttribute("type"),"text", "ERROR: Incorrect atribut.");
+		}
+		
+		@Test
+		public void CreateNewCity() {
+//			navPage.getLoginButton().click();
+			loginPage.getEmailInput().sendKeys("admin@admin.com");
+			loginPage.getPasswordInput().sendKeys("12345");
+			loginPage.getLoginButton().click();
+			navPage.getAdminButton().click();
+			navPage.getCitiesLink().click();
+			citiesPage.getNewItemButton().click();
+			citiesPage.getNewItemInput().sendKeys("Praha");
+			citiesPage.getSaveButtonFromDialog().click();
+			citiesPage.waitForMessageDialogToBeVisible();
+			Assert.assertEquals(citiesPage.getMessageDialogText().getText(), "Saved successfully", "ERROR: Incorrect message.");
+		}
+		
+//		@Test
+//		public void EditCity() {
+//			navPage.getLoginButton().click();
+//			loginPage.getEmailInput().sendKeys("admin@admin.com");
+//			loginPage.getPasswordInput().sendKeys("12345");
+//			loginPage.getLoginButton().click();
+//			navPage.getAdminButton().click();
+//			navPage.getCitiesLink().click();
+//			citiesPage.getSearchInput().sendKeys("Praha");
+//			citiesPage.waitForNumberOfRows(1);
+//			citiesPage.getEditButton(1);
+//			
+//			citiesPage.getSaveButtonFromDialog().click();
+//			
+//			Assert.assertEquals(citiesPage.getTableCell(1, 2).getText(), "Praha", "ERROR");
+//		}
+		
+		@Test
+		public void SearchCity() {
+			loginPage.getEmailInput().sendKeys("admin@admin.com");
+			loginPage.getPasswordInput().sendKeys("12345");
+			loginPage.getLoginButton().click();
+			navPage.getAdminButton().click();
+			navPage.getCitiesLink().click();
+			citiesPage.getSearchInput().sendKeys("Praha");
+			citiesPage.waitForNumberOfRows(1);
+			Assert.assertEquals(citiesPage.getTableCell(1, 2).getText(), "Praha", "ERROR");
+		}
+		
+		@Test
+		public void DeleteCity() {
+			
+		}
 }
